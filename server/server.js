7aeb448bcc4fe2d4ccc9ff5cbc5fb2748c6a6804
smarttech <smarttech@smarttechs-iMac.local>
2017-12-01 20:35:55 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const socketIO = require('socket.io');
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -14,10 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User Connected');
 
-
-  socket.on('disconnect', () => {
-    console.log('User was Disconnected');
+  //socket.emit from Admin text Welcome to chat app
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat App',
+    createdAt: new Date().getTime()
   });
+
+  //socket.broadcast.emit from Admin text new user joined
+  socket.broadcast.emit('newMessage',{
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
@@ -26,6 +35,15 @@ io.on('connection', (socket) => {
       to: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.to,
+    //   createdAt: new Date().getTime()
+    // })
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was Disconnected');
   });
 });
 
