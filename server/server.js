6cfb318d705d8ab45f02a732d18c0c711e7818da
smarttech <smarttech@smarttechs-iMac.local>
2017-12-01 20:05:04 +0530

    init commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+  console.log('New User Connected');
+
+
+  socket.on('disconnect', () => {
+    console.log('User was Disconnected');
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      to: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
